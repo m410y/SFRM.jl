c813ad62e568d8d57f5963086ce71649dde8b6ef
m410y <m410y@proton.me>
2025-04-12 17:16:53 +0700

    v0.1.3

--- src/SFRM.jl
+++ src/SFRM.jl
@@ -17,10 +17,4 @@ include("file.jl")
 include("load.jl")
 include("save.jl")
 
-mktemp() do fpath, _
-    sfrm = load("data/precompile.sfrm")
-    save(fpath, sfrm)
-    Sys.iswindows() && GC.gc()
-end
-
 end # module SFRM
